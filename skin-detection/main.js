fc432f37c7ca8a4d692419faa3b60666339430f1
Mike Moran <mike@houseofmoran.com>
2013-12-21 23:47:38 +0100

    I was right about negative values; I was not seeing them because I was stupidly using an unsigned array. Started on applying simple skin detection by simply ignoring all of the smoothing and texture filter. Still not working, as output is entirely blank, so I suspect I am making a stupid mistake elsewhere

--- skin-detection/main.js
+++ skin-detection/main.js
@@ -1,7 +1,10 @@
 (function (doc, nav) {
     "use strict";
 
-    var video, width, height, zeroResponseContext, logOpponentBuffers = {}, logOpponentContexts = {};
+    var video, width, height,
+        zeroResponseContext,
+        logOpponentBuffers = {}, logOpponentContexts = {},
+        skinBuffer, skinContext;
     var bufidx = 0, buffers = [];
 
     function initialize() {
@@ -15,13 +18,17 @@
         zeroResponseContext = zeroResponseCanvas.getContext("2d");
 
         // log opponent
-        logOpponentBuffers['i'] = new Uint8Array(width * height);
-        logOpponentBuffers['rg'] = new Uint8Array(width * height);
-        logOpponentBuffers['by'] = new Uint8Array(width * height);
+        logOpponentBuffers['i'] = new Int16Array(width * height);
+        logOpponentBuffers['rg'] = new Int16Array(width * height);
+        logOpponentBuffers['by'] = new Int16Array(width * height);
 
         logOpponentContexts['i'] = doc.getElementById("log-opponent-i").getContext("2d");
         logOpponentContexts['rg'] = doc.getElementById("log-opponent-rg").getContext("2d");
         logOpponentContexts['by'] = doc.getElementById("log-opponent-by").getContext("2d");
+
+        // candidate skin pixels
+        skinBuffer = new Uint8Array(width * height);
+        skinContext = doc.getElementById("skin").getContext("2d");
 
         // Get the webcam's stream.
         nav.getUserMedia({video: true}, startStream, function () {});
@@ -43,6 +50,8 @@
             zeroResponseContext.putImageData(frame, 0, 0);
             convertToLogOpponent(frame.data, logOpponentBuffers);
             visualiseLogOpponent(logOpponentBuffers, logOpponentContexts);
+            detectSkin(logOpponentBuffers, skinBuffer);
+            visualiseSkin(skinBuffer, skinContext);
         }
 
         // Wait for the next frame.
@@ -101,25 +110,93 @@
             var b = data[i+2];
             logOpponentBuffers['i'][index] = L(g);
             logOpponentBuffers['rg'][index] = L(r) - L(g);
-            logOpponentBuffers['by'][index] = L(b) - (L(g) + L(r))/2.0;
+            logOpponentBuffers['by'][index] = L(b) - (L(g) + L(r))/2;
         }
     }
 
     function visualiseLogOpponent(logOpponentBuffers, logOpponentContexts)
     {
-        var parts = ['i','rg','by'];
-        parts.forEach(function(part) {
-            var context = logOpponentContexts[part];
+        function findBounds(buffer) {
+            var min = buffer[0];
+            var max = buffer[0];
+            for (var i = 0; i < buffer.length; i++) {
+                var value = buffer[i];
+                min = Math.min(value, min);
+                max = Math.max(value, max);
+            };
+            var range = max - min;
+            return {
+                max: max,
+                min: min,
+                range: range
+            };
+        }
+        function visualiseRange(buffer, context) {
+            var bounds = findBounds(buffer);
             var image = context.createImageData(width, height);
             var data = image.data;
             for (var i = 0; i < data.length; i+= 4) {
-                var value = logOpponentBuffers[part][i / 4];
-                data[i] = data[i + 1] = data[i + 2] = value;
+                var value = buffer[i / 4];
+                var scaled = 256 * ((value - bounds.min) / (bounds.range * 1.0));
+                data[i] = data[i + 1] = data[i + 2] = scaled;
                 data[i + 3] = 255;
             }
             context.putImageData(image, 0, 0);
 //            context.fillRect(10, 10, width - 20, height - 20);
+        }
+
+        var parts = ['i','rg','by'];
+        parts.forEach(function(part) {
+            var context = logOpponentContexts[part];
+            visualiseRange(logOpponentBuffers[part], context);
         });
+    }
+
+    function detectSkin(logOpponentBuffers, skinBuffer) {
+        function hue(i, logOpponentBuffers) {
+            return Math.atan(logOpponentBuffers['rg'][i], logOpponentBuffers['by'][i]);
+        }
+        function saturation (i, logOpponentBuffers) {
+            return Math.sqrt(
+                Math.pow(logOpponentBuffers['rg'][i], 2.0)
+                + Math.pow(logOpponentBuffers['by'][i], 2.0));
+        }
+        function between(value, lower, upper) {
+            return (value >= lower) && (value <= upper);
+        }
+        function skin(i, logOpponentBuffers) {
+//            if (logOpponentBuffers['i'][i] <= 5) {
+                var h = hue(i, logOpponentBuffers);
+                var s = saturation(i, logOpponentBuffers);
+                return (between(h, 110, 150) && between(s, 20, 60))
+                    || (between(h, 130, 170) && between(s, 30, 130));
+//            return between(s, 20, 60)
+//                || between(s, 30, 130);
+//            }
+//            else {
+//                return false;
+//            }
+        }
+
+        for (var i = 0; i < logOpponentBuffers['i'].length; i++) {
+            if (skin(i, logOpponentBuffers)) {
+                skinBuffer[i] = 255;
+            }
+            else {
+                skinBuffer[i] = 0;
+            }
+        }
+    }
+
+    function visualiseSkin(skinBuffer, skinContext) {
+        var image = skinContext.createImageData(width, height);
+        var data = image.data;
+        for (var i = 0; i < data.length; i+= 4) {
+            var value = skinBuffer[i / 4];
+            data[i] = data[i + 1] = data[i + 2] = value;
+            data[i + 3] = 255;
+        }
+        skinContext.putImageData(image, 0, 0);
     }
 
     addEventListener("DOMContentLoaded", initialize);
